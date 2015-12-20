61333df210906f3095356a2bc32675804eeae11a
Daniel Hindi <Daniel@MadainCorp.com>
2015-12-19 19:52:01 -0800

    load plugins and carousel

--- widget/homeCtrl.js
+++ widget/homeCtrl.js
@@ -3,55 +3,68 @@ var counter=0;
 $home.controller('homeCtrl', ['$scope'
     , function ($scope) {
 
-        $scope.logoUrl = "https://s3.amazonaws.com/kaleo.live/mobile/whitelabel/56898399-b378-4ca8-bab7-e75aba7c43a7.png";
-        $scope.images = ["http://rack.3.mshcdn.com/media/ZgkyMDEzLzEyLzAzL2M3L01vYmlsZVBsYXRmLmE5NjIxLmpwZwpwCXRodW1iCTk1MHg1MzQjCmUJanBn/4412ed4b/9e6/MobilePlatforms.jpg"];
+        var viewer;
 
-        for(var i=0;i< $scope.images.length ;i++)
-            $scope.images[i]= buildfire.imageLib.resizeImage($scope.images[i],{width:400});
-
-
-        $scope.appContentId = 123;
-
-        var fn = function(w){
-            buildfire.navigation.navigateTo(w.widgetId,w.instanceId, w.title);
-        };
-        $scope.click=fn;
-
-        var widgets = [
-            { title:"Example", widgetId:'examplePlugin',instanceId:123, icon:"https://lh5.ggpht.com/Q9tsVSAIAQIYHju3mG_1oafAX8sVWHF_rNM-uqvD-7gyedk_k04W_XifaW09VRVINg=w300"}
-            ,{title:"Simple Search", widgetId:'exampleSimpleSearch',instanceId:321, icon:"https://www.bellbanks.com/wp-content/uploads/2013/02/mobile-app-icon.jpg", requiresConnection:true}
-            ,{title:"People", widgetId:'peoplePlugin',instanceId:321, icon:"http://www.allthingsscene.co/wp-content/uploads/2014/07/App-icon.png", requiresConnection:true}
-            ,{title:"Contact Us", widgetId:'contactUsPlugin',instanceId:321, icon:"http://www.codeandlogic.com/media/home/appIcon.png", requiresConnection:true}
-            ,{title:"Text", widgetId:'textPlugin',instanceId:654645, icon:"http://icons.iconarchive.com/icons/fasticon/twitter-square/256/twitter-square-icon.png", requiresConnection:true}
-
-
-        ];
-
-
-        var iconSize=50,circleRadious=40;
-
-        for(var i=0;i< widgets.length ;i++)
-            widgets[i].icon= buildfire.imageLib.resizeImage(widgets[i].icon,{width:iconSize});
-
-        $scope.widgets=widgets;
-
-        function initMenu(){
-
-            var items = document.querySelectorAll('a.circleIcon');
-
-
-
-            for(var i = 0, l = items.length; i < l; i++) {
-                items[i].style.left = (iconSize - circleRadious*Math.cos(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
-
-                items[i].style.top = (iconSize + circleRadious*Math.sin(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
-            }
-
-            document.querySelector('.menu-button').onclick = function(e) {
-                e.preventDefault(); document.querySelector('.circle').classList.toggle('open');
+        buildfire.datastore.getWithDynamicData(function (err, result) {
+            if (!err) {
+                $scope.datastoreInitialized = true;
+            } else {
+                console.error("Error: ", err);
                 return;
             }
+
+            if (result && result.data && !angular.equals({}, result.data)) {
+
+                $scope.data = result.data;
+                $scope.id = result.id;
+                if ($scope.data.content && $scope.data.content.carouselImages)
+                    viewer = new buildfire.components.carousel.view('#carousel',$scope.data.content.carouselImages,'WideScreen');
+
+                if ($scope.data._buildfire && $scope.data._buildfire.plugins && $scope.data._buildfire.plugins.result) {
+                    //var pluginsData = circleLauncherShared.getPluginDetails($scope.data._buildfire.plugins.result, $scope.data._buildfire.plugins.data);
+                    //plugins.loadItems(pluginsData);
+
+                    var iconSize=50,circleRadious=30;
+                    var plugins = result.data._buildfire.plugins.result;
+
+                    var l = plugins.length;
+
+                    for(var i = 0; i < l; i++) {
+                        plugins[i].left = (iconSize - circleRadious*Math.cos(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+
+                        plugins[i].top = (iconSize + circleRadious*Math.sin(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+                    }
+
+                    $scope.plugins = plugins;
+                    $scope.cropImage = buildfire.imageLib.cropImage;
+
+                }
+                else if (!$scope.data._buildfire) {
+                    $scope.data._buildfire = {
+                        plugins: {
+                            dataType: "pluginInstance",
+                            data: []
+                        }
+                    };
+                }
+
+                if (!$scope.data.design) {
+                    $scope.data.design = {
+                        backgroundImage: null,
+                        selectedLayout: 1,
+                        backgroundblur: 0
+                    };
+                }
+
+            }
+
+            if($scope.$$phase)$scope.$apply();
+        });
+
+        $scope.start = function(e) {
+            document.querySelector('.circle').classList.toggle('open');
+            return;
         }
-        setTimeout(initMenu,500);
+
     }
 ]);

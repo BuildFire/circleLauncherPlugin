423b3b69f270ef95b842e0866c85781c525f8f02
Daniel Hindi <Daniel@MadainCorp.com>
2015-12-19 20:41:31 -0800

    fix navigation

--- widget/homeCtrl.js
+++ widget/homeCtrl.js
@@ -5,7 +5,10 @@ $home.controller('homeCtrl', ['$scope'
 
         var viewer;
 
-        buildfire.datastore.getWithDynamicData(function (err, result) {
+        buildfire.datastore.onUpdate(function(obj){bind(null,obj)});
+
+        buildfire.datastore.getWithDynamicData(bind);
+        function bind(err, result) {
             if (!err) {
                 $scope.datastoreInitialized = true;
             } else {
@@ -17,12 +20,15 @@ $home.controller('homeCtrl', ['$scope'
 
                 $scope.data = result.data;
                 $scope.id = result.id;
-                if ($scope.data.content && $scope.data.content.carouselImages)
-                    viewer = new buildfire.components.carousel.view('#carousel',$scope.data.content.carouselImages,'WideScreen');
+
+                if ($scope.data.content && $scope.data.content.carouselImages) {
+                    if(!viewer)
+                        viewer = new buildfire.components.carousel.view('#carousel', $scope.data.content.carouselImages);
+                    else
+                        viewer.loadItems($scope.data.content.carouselImages,false);
+                }
 
                 if ($scope.data._buildfire && $scope.data._buildfire.plugins && $scope.data._buildfire.plugins.result) {
-                    //var pluginsData = circleLauncherShared.getPluginDetails($scope.data._buildfire.plugins.result, $scope.data._buildfire.plugins.data);
-                    //plugins.loadItems(pluginsData);
 
                     var iconSize=50,circleRadious=30;
                     var plugins = result.data._buildfire.plugins.result;
@@ -31,13 +37,18 @@ $home.controller('homeCtrl', ['$scope'
 
                     for(var i = 0; i < l; i++) {
                         plugins[i].left = (iconSize - circleRadious*Math.cos(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
-
                         plugins[i].top = (iconSize + circleRadious*Math.sin(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
                     }
 
                     $scope.plugins = plugins;
                     $scope.cropImage = buildfire.imageLib.cropImage;
-
+                    $scope.click= function(p){
+                        buildfire.navigation.navigateTo({pluginId : p.id
+                            ,instanceId : p.data.instanceId
+                            ,folderName: p.data.pluginType.folderName
+                            ,title: p.data.title
+                        });
+                    }
                 }
                 else if (!$scope.data._buildfire) {
                     $scope.data._buildfire = {
@@ -58,12 +69,11 @@ $home.controller('homeCtrl', ['$scope'
 
             }
 
-            if($scope.$$phase)$scope.$apply();
-        });
+            if(!$scope.$$phase)$scope.$apply();
+        };
 
         $scope.start = function(e) {
             document.querySelector('.circle').classList.toggle('open');
-            return;
         }
 
     }
